92e53b9773db45d4140f257c6e35f36a737917f5
Nurken Seitov <seitovnurken@gmail.com>
2018-05-27 12:38:26 +0600

    Made the hamburger menu

---
+++ js/hamburger-menu.js
@@ -0,0 +1,41 @@
+const openButton = document.querySelector(".hamburger-menu-link__bars");
+const template = document.querySelector("#overlayTemplate").innerHTML;
+const overlay = createOverlay(template);
+
+openButton.addEventListener("click", function() {
+  overlay.open();
+  overlay.setContent(contentElement);
+});
+
+function createOverlay(template) {
+  let fragment = document.createElement('div');
+
+  fragment.innerHTML = template;
+
+  const overlayElement = fragment.querySelector(".hamburger-nav");
+  const contentElement = fragment.querySelector(".hamburger-nav__list");
+  const closeElement = fragment.querySelector(".hamburger-nav__btn-close");
+  
+  fragment = null;
+
+  overlayElement.addEventListener("click", e => {
+    if (e.target === overlayElement) {
+      closeElement.click();
+    }
+  });
+  closeElement.addEventListener("click", () => {
+    document.body.removeChild(overlayElement);
+  });
+
+  return {
+    open() {
+      document.body.appendChild(overlayElement);
+    },
+    close() {
+      closeElement.click();
+    },
+    setContent(content) {
+      contentElement.innerHTML = content;
+    }
+  };
+}
