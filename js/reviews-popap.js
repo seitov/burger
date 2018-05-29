bbd47ef92b694cf194db8523ed2569ee06309925
Nurken Seitov <seitovnurken@gmail.com>
2018-05-30 02:22:27 +0600

    Fixed rhe reviews popap

--- js/reviews-popap.js
+++ js/reviews-popap.js
@@ -1,43 +1,11 @@
-const popapOpen = document.querySelector(".reviews__btn");
-const popap = document.querySelector("#reviewsPopap").innerHTML;
-const popapOverlay = createOverlay(popap);
 
-popapOpen.addEventListener("click", function() {
-  popapOverlay.open();
-  popapOverlay.setContent(contentElement);
-});
+function openPopup(e) {
+  e.preventDefault();
+  $('.popapContainer').toggleClass('popapContainer_active');
+};
 
-function createOverlay(popap) {
-  let fragment = document.createElement('div');
+$('.reviews__list .reviews__btn').on('click', openPopup);
+$('.popapClose').on('click', openPopup);
 
-  fragment.innerHTML = popap;
 
-  const overlayElement = fragment.querySelector(".popapContainer");
-  const contentElement = fragment.querySelector(".popapContent");
-  const closeElement = fragment.querySelector(".popapClose");
-  
-  fragment = null;
-
-  overlayElement.addEventListener("click", e => {
-    if (e.target === overlayElement) {
-      closeElement.click();
-    }
-  });
-  closeElement.addEventListener("click", () => {
-    document.body.removeChild(overlayElement);
-  });
-
-  return {
-    open() {
-      document.body.appendChild(overlayElement);
-    },
-    close() {
-      closeElement.click();
-    },
-    setContent(content) {
-      contentElement.innerHTML = content;
-    }
-  };
-}
-
-// попап 2
+ 
